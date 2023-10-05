0610949f59695bf45a8d386203b3b829e62c1dd5
Hayden Hudson <hayden.h.hudson@oracle.com>
2023-10-05 11:43:53 -0400

    the remainder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import MediaViewer from 'media-viewer-js';
+import {IntlProvider} from 'react-intl';
 
 function App() {
   return (
+    <IntlProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,7 +22,33 @@ function App() {
           Learn React
         </a>
       </header>
+
+
+
+
+
+<div>
+
+
+<MediaViewer
+  mediaViewerProps={{
+    files: [
+      {
+        fileUrl: 'Pain Management Consent.pdf',
+      },
+      {
+        fileUrl: 'Pain Management Consent.pdf',
+        zoom: 200,
+        rotation: 90,
+      }
+    ]
+  }}
+  onLoad={() => {}}
+  multiPageMode
+/>
+</div>
     </div>
+    </IntlProvider>
   );
 }
 
